e2bb9a79d4bf445c1ae9fc3da41c9000ec92448c
Pulasthi Bethmage <87483405+pulabethmage@users.noreply.github.com>
2023-07-17 13:11:48 +0530

    Changes in Card

--- src/nav/Navbar.js
+++ src/nav/Navbar.js
@@ -5,10 +5,10 @@ export default function Navbar() {
     <nav className="">
       <div className="bg-blue-600 h-20 top-0 fixed w-full">
         <div className="flex justify-between items-center max-w-6xl m-auto h-20">
-          <div className="text-2xl text-white font-bold uppercase">
-            Flex Box
+          <div className="text-2xl text-white font-bold uppercase font-display">
+            E Market app
           </div>
-          <ul className="flex justify-around h-20">
+          <ul className="flex justify-around h-20 font-display">
             <li className="p-2 text-gray-300 hover:bg-blue-500 hover:text-white flex items-center">
               <a href="#">HOME</a>
             </li>
